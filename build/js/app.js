2b5c8cbbf77bc043b527393a0ea465a1aad2576a
czf <1462061127@qq.com>
2017-12-12 18:31:12 +0800

    layui-nav-item and layui-nav-child need use element

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,17 @@
-dd
+/** app.js */
+var tab;
+layui.define(['element', 'form', 'table', 'laytpl'], function(exports) {
+    var $ = layui.jquery,
+        element = layui.element,
+        layer = layui.layer,
+        _win = $(window),
+        _doc = $(document),
+        // _body = $('.dodo-body'),
+        form = layui.form,
+        table = layui.table;
+    tab = layui.tab;
+    var app = {
+
+    };
+    exports('app', app);
+});
